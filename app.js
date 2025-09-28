95ab762aa618c0b3b4b298390971f5d27a6fd360
Amr <legithamouda@gmail.com>
2025-09-28 10:22:33 +0400

    feat: Implements logger and errorHandler

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 import express from "express";
 import { productRouter } from "./controllers/products.js";
+import { logger, unknownEndpoint } from "./util/middleware.js";
+import { loggerError } from "./util/logger.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(logger);
 app.use("/api/products", productRouter);
+app.use(unknownEndpoint);
+app.use(loggerError);
 
 export { app };
