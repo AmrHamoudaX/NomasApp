9f5a97f193b6026fb2166066b776d45135a034ee
Amr <legithamouda@gmail.com>
2025-11-26 12:02:24 +0400

    feat: Enabling client-side routing
    Implemented a catch-all route to serve the index.html file

--- backend/app.js
+++ backend/app.js
@@ -7,12 +7,16 @@ import { orderRouter } from "./controllers/orders.js";
 import { loginRouter } from "./controllers/login.js";
 import { orderItemRouter } from "./controllers/orderItems.js";
 import { imageRouter } from "./controllers/images.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.json());
 app.use(logger);
-app.use(express.static("dist"));
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
@@ -20,6 +24,10 @@ app.use("/api/login", loginRouter);
 app.use("/api/orderItems", orderItemRouter);
 app.use("/images", express.static("images"));
 app.use("/api/images", imageRouter);
+app.use(express.static(path.join(__dirname, "dist")));
+app.get("/{*splat}", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+});
 app.use(unknownEndpoint);
 app.use(loggerError);
 
