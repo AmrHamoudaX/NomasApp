ebb2f6860c67583a2de8a3fb36ebd66cd065e5c2
Amr <legithamouda@gmail.com>
2025-12-22 14:41:13 +0400

    feat(admin): Implement Categories service

---
+++ admin/src/services/categories.js
@@ -0,0 +1,19 @@
+import { axiosInstance } from "../axiosInstance";
+
+const baseUrl = "api/categories";
+
+async function getAll() {
+  const response = await axiosInstance.get(baseUrl);
+  return response.data;
+}
+
+async function create(newObject) {
+  const response = await axiosInstance.post(baseUrl, newObject);
+  return response.data;
+}
+
+async function deleteId(id) {
+  const response = await axiosInstance.delete(`${baseUrl}/${id}`);
+  return response.data;
+}
+export default { getAll, create, deleteId };
