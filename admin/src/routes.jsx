14230f013b31bcb606acec7f989d979b9e24a546
Amr <legithamouda@gmail.com>
2025-12-24 08:57:59 +0400

    feat(admin): Creates products page for CRUD operations

--- admin/src/routes.jsx
+++ admin/src/routes.jsx
@@ -1,9 +1,11 @@
 import App from "./App";
 import { LoginForm } from "./components/LoginForm";
 import { Categories } from "./components/Categories";
+import { Products } from "./components/Products";
 const routes = [
   { path: "/", element: <App /> },
   { path: "/login", element: <LoginForm /> },
   { path: "/categories", element: <Categories /> },
+  { path: "/products", element: <Products /> },
 ];
 export default routes;
