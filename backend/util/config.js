77a4e7ac346e0e6a0804d511bc7cddde5b40c970
Amr <legithamouda@gmail.com>
2025-12-29 14:09:57 +0400

    feat(backend): Introduces Supabase for Image Storage

--- backend/util/config.js
+++ backend/util/config.js
@@ -3,5 +3,7 @@ import "dotenv/config";
 const DATABASE_URL = process.env.DATABASE_URL;
 const PORT = process.env.PORT || 3001;
 const SECRET_KEY = process.env.SECRET_KEY;
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_KEY = process.env.SUPABASE_KEY;
 
-export { DATABASE_URL, PORT, SECRET_KEY };
+export { DATABASE_URL, PORT, SECRET_KEY, SUPABASE_URL, SUPABASE_KEY };
