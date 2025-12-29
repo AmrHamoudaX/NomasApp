54bb83bd95d84496e9a07bd1daf6b518138e0453
Amr <legithamouda@gmail.com>
2025-12-29 14:14:52 +0400

    refactor(backend): Creates image in db and uploads it to supabase
    - Uses `multer` to process `multipart/form-data` requests.
    - Uploads the image file to Supabase Storage.
    - Creates a new `Image` record in the database with the public URL of
    the uploaded image.
    - Secures the endpoint by requiring admin authentication.

--- backend/controllers/images.js
+++ backend/controllers/images.js
@@ -1,7 +1,56 @@
 import { Router } from "express";
-import { Product, Image } from "../models/index.js";
+import { supabase } from "../util/supabase.js";
+import multer from "multer";
+import { Image } from "../models/index.js";
+import { requireAdmin, tokenExtractor } from "../util/middleware.js";
 
 const router = Router();
+
+//BEGINNING OF MULTER AND SUPABASE SETUP
+const storage = multer.memoryStorage(); //Store uploaded files in memory
+const upload = multer({ storage: storage }); //Initialize multer with the storage config
+router.post(
+  "/",
+  tokenExtractor,
+  requireAdmin,
+  upload.single("file"),
+  async (req, res) => {
+    // Using upload.single("file"), Multer accepts a single file with the given fieldname ("file") and stores it in req.file.
+    const file = req.file;
+    try {
+      if (!file) {
+        console.log(req.file);
+        res.status(400).json({ message: "Please upload a file" });
+        return;
+      }
+      //upload the file to supabase
+      const { data, error } = await supabase.storage
+        .from("images")
+        .upload(file.originalname, file.buffer, {
+          contentType: file.mimetype,
+        });
+      if (error) {
+        console.log(data);
+        throw error;
+      }
+      //get public url of the uploaded file
+      const { data: image } = supabase.storage
+        .from("images")
+        .getPublicUrl(data.path);
+
+      const postImage = await Image.create({
+        productId: req.body.productId,
+        imageurl: image.publicUrl,
+      });
+      res.status(200).json({ postImage });
+    } catch (error) {
+      console.log(req.body);
+      console.log(req.file);
+      res.status(500).json({ error: error.message });
+    }
+  },
+);
+//END OF MULTER AND SUPABASE SETUP
 
 const imageFinder = async (req, res, next) => {
   req.image = await Image.findByPk(req.params.id);
@@ -11,15 +60,6 @@ const imageFinder = async (req, res, next) => {
 router.get("/", async (req, res) => {
   const images = await Image.findAll();
   res.json(images);
-});
-
-router.post("/", async (req, res, next) => {
-  try {
-    const image = await Image.create(req.body);
-    res.json(image);
-  } catch (error) {
-    next(error);
-  }
 });
 
 router.get("/:id", imageFinder, async (req, res) => {
