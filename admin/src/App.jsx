1f1aa3e67a65c618c089bfb5f2a653b2a50dacc4
Amr <legithamouda@gmail.com>
2026-01-08 08:35:02 +0400

    refactor(admin): Implement nested routing with a persistent App layout

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,14 @@
+import { Outlet } from "react-router-dom";
 import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
-    <>
+    <div className="min-h-screen bg-gray-50">
       <Sidebar />
-    </>
+      <main className="flex-1 bg-gray-50 lg:ml-64 ">
+        <Outlet />
+      </main>
+    </div>
   );
 }
 
--- admin/src/routes.jsx
+++ admin/src/routes.jsx
@@ -1,10 +1,18 @@
 import App from "./App";
 import { LoginForm } from "./components/LoginForm";
+import Orders from "./components/Orders";
 import ProductManagement from "./components/ProductManagement";
 
 const routes = [
-  { path: "/", element: <App /> },
-  { path: "/login", element: <LoginForm /> },
-  { path: "/products", element: <ProductManagement /> },
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <ProductManagement /> },
+      { path: "/login", element: <LoginForm /> },
+      { path: "/orders", element: <Orders /> },
+    ],
+  },
 ];
+
 export default routes;
