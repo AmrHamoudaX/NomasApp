26778b06707c3c81baeb14a25da9b5c7aa679289
Amr <legithamouda@gmail.com>
2026-01-31 17:16:29 +0400

    feat: API Endpoints for Admins to Select & Display Featured Products

--- backend/controllers/products.js
+++ backend/controllers/products.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { Product, OrderItem, Image, Order } from "../models/index.js";
 import { requireAdmin, tokenExtractor } from "../util/middleware.js";
 import { supabase } from "../util/supabase.js";
+import { redis } from "../util/redis.js";
 
 const router = Router();
 
@@ -21,6 +22,7 @@ const productFinder = async (req, res, next) => {
   next();
 };
 
+//Get All Products
 router.get("/", async (req, res) => {
   const products = await Product.findAll({
     include: [
@@ -45,6 +47,95 @@ router.post("/", tokenExtractor, requireAdmin, async (req, res, next) => {
     next(error);
   }
 });
+
+// -------Admin Chooses Featured Products------------
+router.put(
+  "/featured",
+  tokenExtractor,
+  requireAdmin,
+  async (req, res, next) => {
+    try {
+      const { productIds } = req.body;
+
+      if (!Array.isArray(productIds)) {
+        return res.status(400).json({ error: "productIds must be an array" });
+      }
+
+      if (productIds.length > 12) {
+        return res.status(400).json({ error: "Too many featured products" });
+      }
+
+      //atomic replace
+      await redis
+        .multi()
+        .del("homepage:featured")
+        // productIds Must be a String
+        .rPush("homepage:featured", productIds)
+        .exec();
+
+      res.json({ success: true });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "Failed to save feature products" });
+    }
+  },
+);
+
+// Get Featured Products
+router.get("/featured", async (req, res) => {
+  try {
+    const ids = await redis.lRange("homepage:featured", 0, -1);
+    console.log(ids);
+    if (!ids.length) {
+      return res.json([]);
+    }
+
+    const featuredProducts = await Product.findAll({
+      where: {
+        id: ids,
+      },
+    });
+    const featuredProductIds = Object.values(featuredProducts).map(
+      // Turn to String!!!
+      (product) => `${product.id}`,
+    );
+
+    console.log(featuredProductIds);
+    res.json(featuredProductIds);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+// Delete FeaturedProduct Id
+router.delete(
+  "/featured/:id",
+  tokenExtractor,
+  requireAdmin,
+  async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      //Remove ID from Redis List
+      const removedCount = await redis.lRem(
+        "homepage:featured",
+        0,
+        id.toString(),
+      );
+      if (removedCount === 0) {
+        return res.status(400).json({ error: "Product not featured" });
+      }
+      res.json({
+        message: `Featured product removed successfully`,
+        productId: id,
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  },
+);
+//--------End Featured Products-------
 
 router.get("/:id", productFinder, async (req, res) => {
   if (req.product) {
